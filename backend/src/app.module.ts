15aa334157c699303c3c2ff1f7645f73bc3db040
Augusto240 <jose.aug18@outlook.com>
2025-12-06 11:02:58 -0300

    feat(backend): adiciona AuthModule e ConfigModule ao app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, TasksModule],
+  imports: [PrismaModule, TasksModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
